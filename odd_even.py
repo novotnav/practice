415ba3caa8a2bc4edb2c84d1fb94952b3de9e3c8
Veronika Novotna <veronika.novotna@protonmail.com>
2019-08-08 09:23:59 +0200

    refactoring

--- odd_even.py
+++ odd_even.py
@@ -1,8 +1,8 @@
 print("Let's find out if a number is odd or even.")
 nr = int(input("Tell me some number: "))
-remainder1 = nr % 2
+rem1 = nr % 2
 
-if remainder1 is 0:
+if rem1 is 0:
     print("This number is even.")
 else:
     print("This number is odd.")
@@ -10,9 +10,9 @@ else:
 print("Now let's find out if one number is divisible by another one with or without remainder.")
 first = int(input("Tell me the first number: "))
 second = int(input("Tell me the second number: "))
-remainder2 = first % second
+rem2 = first % second
 
-if remainder2 is 0:
+if rem2 is 0:
     print("The first number is divisible by the second number without remainder.")
 else:
-    print("The first number is divisible by the second number with remainder: " + str(remainder2) + ".")
+    print("The first number is divisible by the second number with remainder: " + str(rem2) + ".")
