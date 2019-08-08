8474dce82dc3adb2b28d73af60f7f353b53857cc
Veronika Novotna <veronika.novotna@protonmail.com>
2019-08-08 11:24:12 +0200

    creating functions

--- odd_even.py
+++ odd_even.py
@@ -1,18 +1,22 @@
 print("Let's find out if a number is odd or even.")
 nr = int(input("Tell me some number: "))
-rem1 = nr % 2
 
-if rem1 is 0:
-    print("This number is even.")
-else:
-    print("This number is odd.")
+def oe():
+    rem1 = nr % 2
+    if rem1 is 0:
+        print("This number is even.")
+    else:
+        print("This number is odd.")
+oe()
 
 print("Now let's find out if one number is divisible by another one with or without remainder.")
 first = int(input("Tell me the first number: "))
 second = int(input("Tell me the second number: "))
-rem2 = first % second
 
-if rem2 is 0:
-    print("The first number is divisible by the second number without remainder.")
-else:
-    print("The first number is divisible by the second number with remainder: " + str(rem2) + ".")
+def divis():
+    rem2 = first % second
+    if rem2 is 0:
+        print("The first number is divisible by the second number without remainder.")
+    else:
+        print("The first number is divisible by the second number with remainder: " + str(rem2) + ".")
+divis()
